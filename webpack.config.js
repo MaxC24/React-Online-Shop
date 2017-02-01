7c3630c7abfb6f7554c113e5c1a1b6051a94e4b9
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-02-01 14:20:37 -0500

    creates folder structure and working webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    devtool: "inline-sourcemap",
+	entry: path.join(__dirname, '/client/src/app.jsx'),
+
+	output: {
+		path: path.join(__dirname, '/client/dist/js'),
+		filename: 'app.js'
+	},
+
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			include: path.join(__dirname, 'client/src'),
+			loader: 'babel',
+			query:{
+				presets:["react", "es2015"]
+			}
+		}]
+	},
+	watch: true
+}
