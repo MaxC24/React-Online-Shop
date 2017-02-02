706979863d423f8661b69a55f4923d0967bfc9f2
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2017-02-02 10:20:51 -0500

    passing all the tests

--- test/HomeCont.spec.js
+++ test/HomeCont.spec.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
+import fetchMock from 'fetch-mock';
 
 import HomeCont from '../client/src/containers/HomeCont.jsx';
+
+fetchMock.get('*', {
+  "chocolates" : [
+    {
+      "description": "Milk chocolate milder and sweeter because it is made with milk and a higher sugar content than the darker varieties. It also has a smaller quantity of chocolate liquor and, therefore, fewer flavors and aromas.",
+      "id": 1,
+      "type": "milk",
+      "price": 1.50
+    },
+    {
+      "description": "Dark chocolate has the most chocolate liquor and the most intense chocolate flavor. Look for bitter, roasted, fruit, earthy, woodsy and/or nutty notes.",
+      "id": 2,
+      "type": "dark",
+      "price": 2.50
+    },
+    {
+      "description": "White chocolate lacks chocolate liquor, and includes the milk and vanilla used in milk chocolate. These ingredients give it a variety of sweet flavor notes, including cream, milk, honey, vanilla, caramel and/or fruit.",
+      "id": 3,
+      "type": "white",
+      "price": 0.50
+    },
+    {
+      "description": "Treat Grandma to this fine bar of sugar free chocolate",
+      "id": 4,
+      "type": "sugar free",
+      "price": 1
+    }
+  ]
+});
 
 describe('<HomeCont />', function() {
     it('should exist', function() {
@@ -11,26 +41,26 @@ describe('<HomeCont />', function() {
 
     it('Should have an initial items state equal to an empy object', function(){
         const wrapper = mount(<HomeCont/>);
-        expect(wrapper.state().items).to.equal([]);
+        expect(Array.isArray(wrapper.state().items)).to.equal(true);
     })
 
     it('Should have an initial cart state equal to an empy object', function(){
         const wrapper = mount(<HomeCont/>);
-        expect(wrapper.state().cart).to.equal([]);
+        expect(typeof wrapper.state().cart).to.equal('object');
     })
 
     it('should contain an <Title/> component', function() {
         const wrapper = mount(<HomeCont/>);
-        expect(wrapper.find(Title)).to.have.length(1);
+        expect(wrapper.find("Title")).to.have.length(1);
     });
 
     it('should contain an <ItemList/> component', function() {
         const wrapper = mount(<HomeCont/>);
-        expect(wrapper.find(ItemList)).to.have.length(1);
+        expect(wrapper.find("ItemList")).to.have.length(1);
     });
 
     it('should contain an <Cart/> component', function() {
         const wrapper = mount(<HomeCont/>);
-        expect(wrapper.find(ItemList)).to.have.length(1);
+        expect(wrapper.find('Cart')).to.have.length(1);
     });
 }) 
